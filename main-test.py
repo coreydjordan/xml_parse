469f78ba4406fd1c72e0b4b93ad1abfd6dcc5c6e
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2023-03-14 10:38:51 -0500

    started writing a new function in main-test that is more universal and not static.

--- main-test.py
+++ main-test.py
@@ -30,35 +30,52 @@ for test_ch in test_root.iter():
 #     print("ERROR REPORT: TAGS WITH NO DATA: " + tags)
 
 
+'''
+This recursion function takes one parameter, it gets the length of the xml file and does a loop within that range. if the tag is found, it will append the .text from that tag to a list called "text". If it finds a tag that has children (this is static for the moment) then it will run the recrsion on with that section of the xml as the new parameter, it will run through the whole function again. 
 
+The plan is to append all the values that come out of this function to a list, then do the same by plugging in the other xml file as the parameter, then comparing the lists to see if they all have the same values. 
+'''
 
+# def recursion_xml(file):
+#     text = ""
+#     current_file_length = len(file)
+#     for i in range(current_file_length):
+#         if file[i].tag == "Id":
+#             print("Id checked")
+#             text += f"{file[i].text}"
+#         if file[i].tag == "Client":
+#             print("Client checked")
+#             parent = file[i]
+#             text += recursion_xml(parent)
+#         elif file[i].tag == "Job":
+#             print("Job checked")
+#             parent = file[i]
+#             text += recursion_xml(parent)
+#             #not sure how to check double nested elements.
+#         # elif file[i].tag == "Comepensation":
+#         #     print("Comp checked")
+#         #     parent = file[i]
+#         #     print(parent)
+#         #     text += recursion_xml(parent)
+#         elif file[i].tag == "Frequency":
+#             #frequency = None
+#             print("Frequency checked")
+#             text += f" {file[i].text}"
+#     return text
 
+# print(recursion_xml(gold_root))
 
-def recursion_xml(file):
+#finds out if there is nested items in the xml
+# for i in gold_root.iter():
+#     if len(i) > 0:
+#         print(i.tag)
+        
+        
+def recursion_xml(current_node):
     text = []
-    current_file_length = len(file)
-    for i in range(current_file_length):
-        if file[i].tag == "Id":
-            print("Id checked")
-            text.append(file[i].text)
-        if file[i].tag == "Client":
-            print("Client checked")
-            parent = file[i]
-            text.append(recursion_xml(parent))
-        elif file[i].tag == "Job":
-            print("Job checked")
-            parent = file[i]
-            text.append(recursion_xml(parent))
-            #not sure how to check double nested elements.
-        # elif file[i].tag == "Comepensation":
-        #     print("Comp checked")
-        #     parent = file[i]
-        #     print(parent)
-        #     text += recursion_xml(parent)
-        elif file[i].tag == "Frequency":
-            #frequency = None
-            print("Frequency checked")
-            text.append(file[i].text)
+    current_file_length = len(current_node)
+    for n in range(current_file_length):
+        print(current_node.tag)
     return text
 
 print(recursion_xml(gold_root))
