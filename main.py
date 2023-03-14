7a9b0d502f110a26a74ab9e27bf10b34997f89ab
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2023-03-14 13:35:32 -0500

    deleted main.py and made main-test.py the main file. Removed some extra code I had when testing for the proper way to write the function. Removed some comments that were no longer needed.

--- main.py
+++ main.py
@@ -1,68 +1,46 @@
-#ensure data is present in the tag
-#*if there is data, compare it to the other file
-#*no match = False
-#!find a way to return false and move on to the next conditional to check that xml property
-#!RECURSION!
-#if n == len(xml_file) return <-- terminate the recrusion with this
-#//ensure TEMPLATE_ID and MARKET_ID are the same before comparing
-#!compare test file against GOLD file to see differences.
-#how to check is tag has nested data?
-
 import xml.etree.ElementTree as ET
 import logging as lg
+import pprint as pp
 
-#elem1 and elem2 are paramaters that we are filling in when we call the function
-def elements_equal(elem1, elem2):
-    # for child in elem1.iter():
-    #     ch_tag = child.tag
-    #     ch_text = child.text
-    # for child2 in elem2.iter():
-    #     ch2_tag = child2.tag
-    #     ch2_text = child2.text
-    if elem1.tag != elem2.tag:
-        print(elem1.tag + " and " + elem2.tag + " do not match")
-        return False
-    if elem1.text != elem2.text:
-        print(elem1.text + " and " + elem2.text + " do not match")
-        if  elem1.text != None and elem2.text != None:
-            lg.warning("Both elem1 and elem2 text are None")
-#             #this is where the two files differ for the first time
-            return False
-#     #not 100% sure what tail is but I went ahead and made conditionals to check  everything in the ET documentation
-    if elem1.tail != elem2.tail:
-        print(elem1.tail + " and " + elem2.tail + " do not match")
-        if elem1.tail != None and elem2.tail != None:
-            lg.warning("Both elem1 and elem2 tail are None")
-            return False
-    if elem1.attrib != elem2.attrib:
-        print(elem1.attrib + " and " + elem2.attrib + " do not match")  
-        return False
-#     #all() returns true if ALL items in the iteration are true, otherwise it returns false. if the iteration object is empty, all() also returns true
-    return all(elements_equal(c1, c2) for c1, c2 in zip(elem1, elem2))
+#parse xml files
+gold_tree = ET.parse('GOLD_file.xml')
+test_tree = ET.parse('test.xml')
+sample_tree = ET.parse('sample_1.xml')
+gold_root = gold_tree.getroot()
+test_root = test_tree.getroot()
+sample_root = sample_tree.getroot()
 
-def parse_xml(f1, f2):
-    tree_elem1 = ET.parse(f1)
-    root1 = tree_elem1.getroot()
-    tree_elem2 = ET.parse(f2)
-    root2 = tree_elem2.getroot()
-    #find all returns a list that has to be iterated over even though there's only 1 item in the list
-    # for ch in root2.findall("./MARKET_ID"):
-    #     #set the text from the market_id tag to a variable
-    #     market_id_2 = ch.text
-    # for ch in root1.findall("./MARKET_ID"):
-    #     market_id_1 = ch.text
-    # #compare the two variables
-    # if market_id_2 == market_id_1:
-    #     print("TRUE")
-    # else:
-    #     print("FALSE")
+#step 3
+empty_tags = []
+for test_ch in test_root.iter():
+    if test_ch.text == None:
+        empty_tags.append(test_ch.tag)       
+for tags in empty_tags:
+    print("ERROR REPORT: TAGS WITH NO DATA: " + tags)
+
+
+'''
+This recursion function takes one parameter, it gets the length of the xml file and does a loop within that range. if the tag is found, it will append the .text from that tag to a list called "text". If it finds a tag that has children (this is static for the moment) then it will run the recrsion on with that section of the xml as the new parameter, it will run through the whole function again. 
+
+The plan is to append all the values that come out of this function to a list, then do the same by plugging in the other xml file as the parameter, then comparing the lists to see if they all have the same values. 
+'''
+        
+        
+def recursion_xml(current_node):
+    text = []
+    current_file_length = len(current_node)
+    for n in range(current_file_length):
+        #trying to find any nodes that have nested elements
+        if len(current_node[n]) > 0:
+            #only returns Client and Job, skips over Compensation for some reason
+            parent = current_node[n]
+            text.append(recursion_xml(parent))
+        if len(current_node[n]) == 0:
+            text.append(current_node[n].tag)
     
+    return text
 
-    #function call
-    return elements_equal(root1,root2)
+# print(recursion_xml(gold_root))
+print(recursion_xml(test_root))
 
-# #declaring the two sample xmls as f1 and f2 variables
-f1 = 'GOLD_file.xml'
-f2 = 'test.xml'
-# #function call
-print(parse_xml(f1, f2))
+#its putting nested nodes in a nested list
