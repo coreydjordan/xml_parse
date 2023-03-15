5c933d51d81479b0d71ff6099b0be1e5c1dc9249
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2023-03-15 10:08:13 -0500

    added logic to check for empty tags and output an error report.

--- main.py
+++ main.py
@@ -10,21 +10,15 @@ gold_root = gold_tree.getroot()
 test_root = test_tree.getroot()
 sample_root = sample_tree.getroot()
 
-#step 3
-empty_tags = []
-for test_ch in test_root.iter():
-    if test_ch.text == None:
-        empty_tags.append(test_ch.tag)       
-for tags in empty_tags:
-    print("ERROR REPORT: TAGS WITH NO DATA: " + tags)
-
-
 '''
 This recursion function takes one parameter, it gets the length of the xml file and does a loop within that range. if the tag is found, it will append the .node_list from that tag to a list called "node_list". If it finds a tag that has children (this is static for the moment) then it will run the recrsion on with that section of the xml as the new parameter, it will run through the whole function again. 
 
 The plan is to append all the values that come out of this function to a list, then do the same by plugging in the other xml file as the parameter, then comparing the lists to see if they all have the same values. 
+
+Make sure MARKET_ID and LETTER_TYPE match
+
+Iterate over all files in database and plugging them into the parameters of the function
 '''
-        
         
 def recursion_xml_1(current_node):
     node_list = []
@@ -39,13 +33,14 @@ def recursion_xml_1(current_node):
             node_list.append(current_node[n].tag)
     return node_list
 
-print(recursion_xml_1(gold_root))
-
-#its putting nested nodes in a nested list
-
 def recursion_xml_2(current_node):
     node_list_2 = []
     current_file_length = len(current_node)
+    #shows the path of nodes that have no data but its iterating too many times, need to only loop once
+    # for m in current_node.iter():
+    #     if m.text == None:
+    #         print("ERROR: MISSING DATA: " + str(test_root.find("./[@source='DH']").tag) + "/" + str(m.tag))
+    #         continue
     for n in range(current_file_length):
         #trying to find any nodes that have nested elements and those nodes are not empty
         if len(current_node[n]) > 0 and current_node[n].text != None:
@@ -55,8 +50,9 @@ def recursion_xml_2(current_node):
         if len(current_node[n]) == 0 and current_node[n].text != None:
             list_info = current_node[n].tag, current_node[n].text
             node_list_2.append(list_info)
-    source = str(test_root.find("./[@source='DH']"))
-    print(source + "/" + str(current_node[n]))
-    return node_list_2
+    for elemnt in node_list_2:
+        print(elemnt)
+    
+    return 
 
 print(recursion_xml_2(test_root))
