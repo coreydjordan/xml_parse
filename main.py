34a6c822b6ca3e424e30f1f1c80382664bf62cd4
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2023-03-15 14:48:54 -0500

    made changes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import xml.etree.ElementTree as ET
-import logging as lg
-import pprint as pp
 
 #parse xml files
 gold_tree = ET.parse('GOLD_file.xml')
@@ -8,7 +6,6 @@ test_tree = ET.parse('test.xml')
 sample_tree = ET.parse('sample_1.xml')
 gold_root = gold_tree.getroot()
 test_root = test_tree.getroot()
-sample_root = sample_tree.getroot()
 
 '''
 This recursion function takes one parameter, it gets the length of the xml file and does a loop within that range. if the tag is found, it will append the .node_list from that tag to a list called "node_list". If it finds a tag that has children (this is static for the moment) then it will run the recrsion on with that section of the xml as the new parameter, it will run through the whole function again. 
@@ -21,18 +18,23 @@ Iterate over all files in database and plugging them into the parameters of the 
 '''
         
 def recursion_xml_1(current_node):
+    param_list = ["GOLD.xml", "test.xml"]
+    #!xpath to the market_id node
     e_gold = gold_root.findall("Market_ID")
+    #!iterate over the e_gold variable because it is a list
     for i_gold in e_gold:
+        #!set the term_gold as a global variable so it can be accessed outside of the function
         global term_gold
         term_gold = i_gold.text
+        #!if the Market_ID from the test.xml and gold.xml do not match, an exception will be raised and the program will stop
     if term_test != term_gold:
         raise Exception("The Market IDs do not match")
     node_list = []
     current_file_length = len(current_node)
     for n in range(current_file_length):
-        #trying to find any nodes that have nested elements 
+        #!trying to find any nodes that have nested elements 
         if len(current_node[n]) > 0 and current_node[n].text != None:
-            #only returns Client and Job, skips over Compensation for some reason
+            #!only returns Client and Job, skips over Compensation for some reason
             parent = current_node[n]
             node_list.append(recursion_xml_1(parent))
         if len(current_node[n]) == 0 and current_node[n].text != None:
@@ -43,7 +45,10 @@ def recursion_xml_1(current_node):
     return node_list
 
 def recursion_xml_2(current_node):
-    #first make sure the market_ids match
+    # param_list = ["GOLD.xml", "test.xml"]
+    # for param in param_list:
+    #     print(param)
+    #!first make sure the market_ids match
     e_test = test_root.findall("Market_ID")
     for i in e_test:
         global term_test 
@@ -56,28 +61,21 @@ def recursion_xml_2(current_node):
     #         print("ERROR: MISSING DATA: " + str(test_root.find("./[@source='DH']").tag) + "/" + str(m.tag))
     #         continue
     for n in range(current_file_length):
-        #trying to find any nodes that have nested elements and those nodes are not empty
+        #!trying to find any nodes that have nested elements and those nodes are not empty
         if len(current_node[n]) > 0 and current_node[n].text != None:
             #only returns Client and Job, skips over Compensation for some reason
             parent = current_node[n]
             node_list_2.append(recursion_xml_2(parent))
+            print("recursion checked")
         if len(current_node[n]) == 0 and current_node[n].text != None:
             list_info = current_node[n].tag, current_node[n].text
             node_list_2.append(list_info)
     for elemnt in node_list_2:
-        print(elemnt)
+        # print(elemnt)
+        pass
     
     return node_list_2
 
 recursion_xml_2(test_root)
-recursion_xml_1(gold_root)
+# recursion_xml_1(gold_root)
 
-# def get_text():
-#     e = test_root.findall("Market_ID")
-#     for i in e:
-#         term = i.text
-#     return term
-
-# print(get_text())
-
-print(term_gold, term_test)
